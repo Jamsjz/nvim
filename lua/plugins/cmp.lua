3ec3335b7b6c88efbcda8d03d961b7f51a1fe319
Your Name <gj984233@gmail.com>
2024-06-22 01:54:00 +0545

    new

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,3 @@
-
 return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
@@ -9,26 +8,28 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-cmdline",
     "saadparwaiz1/cmp_luasnip",
+    "nvim-lua/plenary.nvim",
     "L3MON4D3/LuaSnip",
     "rafamadriz/friendly-snippets",
     "onsails/lspkind.nvim",
-     {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-      },
+    {
+      "Exafunction/codeium.nvim",
+      cmd = "Codeium",
+      build = ":Codeium Auth",
+      opts = {},
+      config = function()
+        require("codeium").setup({})
+      end,
+    },
   },
-      ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "codeium",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-
-
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 100,
+    })
+  end,
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
@@ -40,56 +41,41 @@ return {
         completeopt = "menu,menuone,preview,noselect",
       },
       snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
+          luasnip.lsp_expand(args.body)
         end,
       },
-
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       },
-
       mapping = cmp.mapping.preset.insert({
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-      }), -- end of mapping section
-
-      -- Managing Sources for completions
+      }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "codeium" },
-        { name = "luasnip" }, -- For luasnip users.
+        { name = "luasnip" },
         { name = "buffer" },
+        { name = "path" },
       }),
-
       formatting = {
         format = lspkind.cmp_format({ with_text = true, maxwidth = 50 }),
       },
-         sources = {
-    {
-      name = 'path',
-      option = {
-        -- Options go into this table
-      },
-    },
-  },
     })
 
-    -- Set configuration for specific filetype.
     cmp.setup.filetype("gitcommit", {
       sources = cmp.config.sources({
-        { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+        { name = "cmp_git" },
       }, {
         { name = "buffer" },
       }),
     })
 
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
@@ -97,7 +83,6 @@ return {
       },
     })
 
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
@@ -107,7 +92,7 @@ return {
       }),
     })
 
-    --  VS Code like snippets
     vscodesnippets.lazy_load()
   end,
 }
+ 
