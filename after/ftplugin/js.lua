7fb69781e3d2be7207fd32e3b417e502deb4aa3a
Jamsjz <gj984233@gmail.com>
2025-02-09 01:31:36 +0545

    new

--- after/ftplugin/js.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.shiftwidth = 2 -- Set number of spaces for each indentation level
-vim.opt.tabstop = 2 -- Set number of spaces a tab counts for
-vim.opt.expandtab = true -- Convert tabs to spaces
