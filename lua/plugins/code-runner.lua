b3c6d2fafdc5d92b843af8d60e28c0ef8aece2a7
Jamsjz <gj984233@gmail.com>
2025-02-19 12:30:41 +0545

    new

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -4,7 +4,7 @@ return {
 
   opts = {
     filetype = {
-      javascript = "node",
+      javascript = "node $fileName",
       go = "go run .",
       typescript = {
         "cd $dir &&",
--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -1,36 +1,49 @@
 return {
-  "adalessa/laravel.nvim",
-  dependencies = {
-    "tpope/vim-dotenv",
-    "nvim-telescope/telescope.nvim",
-    "MunifTanjim/nui.nvim",
-    "kevinhwang91/promise-async",
-  },
-  cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-  keys = {
-    { "<leader>la", ":Laravel artisan<cr>" },
-    { "<leader>lr", ":Laravel routes<cr>" },
-    { "<leader>lm", ":Laravel related<cr>" },
-  },
-  event = { "VeryLazy" },
-  opts = {
-    features = {
-      null_ls = {
-        enable = true,
-      },
-      route_info = {
-        enable = true, --- to enable the laravel.nvim virtual text
-        middlewares = true, --- wheather to show the middlewares section in the info
-        method = true, --- wheather to show the method section in the info
-        uri = true, --- wheather to show the uri section in the info
-      },
-      model_info = {
-        enable = true, --- to enable the laravel.nvim virtual text
-      },
-      overide = {
-        enable = true,
-      },
+  {
+    "ricardoramirezr/blade-nav.nvim",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    ft = { "blade", "php" },
+    opts = {
+      close_tag_on_complete = false,
     },
   },
-  config = true,
+
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    opts = {
+      features = {
+        null_ls = {
+          enable = true,
+        },
+        route_info = {
+          enable = true, --- to enable the laravel.nvim virtual text
+          middlewares = true, --- wheather to show the middlewares section in the info
+          method = true, --- wheather to show the method section in the info
+          uri = true, --- wheather to show the uri section in the info
+        },
+        model_info = {
+          enable = true, --- to enable the laravel.nvim virtual text
+        },
+        overide = {
+          enable = true,
+        },
+      },
+    },
+    config = true,
+  },
 }
